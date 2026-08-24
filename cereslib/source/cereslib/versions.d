46aee3cd5f3b028c9bfec6c07928071a9b09897c
yoz75 <levtannn@gmail.com>
2026-08-24 14:27:59 +0300

    Update versions.d

--- cereslib/source/cereslib/versions.d
+++ cereslib/source/cereslib/versions.d
@@ -29,11 +29,68 @@ public struct Version
     }
 
     /// Get version's representation as string
-    /// Returns: 
-    public string toString() shared const pure
+    /// Returns: string representation of version
+    public string toString() inout pure
     {
         import std.conv : to;
 
         return major.to!string ~ '.' ~ minor.to!string ~ '.' ~ patch.to!string;
+    }
+
+    /// Get version's representation as wstring
+    /// Returns: wstring representation of version
+    public wstring toWstring() inout pure
+    {
+        import std.conv : to;
+
+        return major.to!wstring ~ '.' ~ minor.to!wstring ~ '.' ~ patch.to!wstring;
+    }
+
+    /// Get version's representation as dstring
+    /// Returns: dstring representation of version
+    public dstring toDstring() inout pure
+    {
+        import std.conv : to;
+
+        return major.to!dstring ~ '.' ~ minor.to!dstring ~ '.' ~ patch.to!dstring;
+    }
+
+        /// Get version's representation as string
+    /// Returns: string representation of version
+    public string toString() shared inout pure
+    {
+        import std.conv : to;
+
+        size_t majorLocal = this.major;
+        size_t minorLocal = this.minor;
+        size_t patchLocal = this.patch;
+
+        return majorLocal.to!string ~ '.' ~ minorLocal.to!string ~ '.' ~ patchLocal.to!string;
+    }
+
+    /// Get version's representation as string
+    /// Returns: string representation of version
+    public wstring toWstring() shared inout pure
+    {
+        import std.conv : to;
+
+        size_t majorLocal = this.major;
+        size_t minorLocal = this.minor;
+        size_t patchLocal = this.patch;
+
+        return majorLocal.to!wstring ~ '.' ~ minorLocal.to!wstring ~ '.' ~ patchLocal.to!wstring;
+    }
+
+    /// Get version's representation as string
+    /// Returns: string representation of version
+    public dstring toDstring() shared inout pure
+    {
+        import std.conv : to;
+
+        size_t majorLocal = this.major;
+        size_t minorLocal = this.minor;
+        size_t patchLocal = this.patch;
+
+        return majorLocal.to!dstring ~ '.' ~ minorLocal.to!dstring ~ '.' ~ patchLocal.to!dstring;
     }
 }
