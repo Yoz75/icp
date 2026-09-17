b866dc6f4945b6d43a2d9083a24b8ab776d4379d
yoz75 <levtannn@gmail.com>
2026-09-17 17:54:09 +0300

    Create properties.d

---
+++ cereslib/source/cereslib/properties.d
@@ -0,0 +1,82 @@
+module cereslib.properties;
+import std.traits : getUDAs, Unqual;
+
+struct min(T)
+{
+    T value;
+}
+
+struct max(T)
+{
+    T value;
+}
+
+
+/// Template for fast getter maker. 
+/// Params:
+///   Field = the field
+///   name = the name of the getter. Default is the `Field`s name without the last character
+template MakeGetter(alias Field, string name = __traits(identifier, Field)[0..$-1])
+{
+    enum fieldName = __traits(identifier, Field);
+
+    mixin("@property auto " ~ name ~ "() => " ~ fieldName ~ ";"
+    );
+}
+
+
+template MakeSetter(alias Field, string name = __traits(identifier, Field)[0..$-1])
+{
+    import std.traits : getUDAs, Unqual;
+    enum fieldName = __traits(identifier, Field);
+    alias FieldType = typeof(Field);
+
+    enum hasMin = getUDAs!(Field, cereslib.properties.min!FieldType).length != 0;
+    enum hasMax = getUDAs!(Field, cereslib.properties.max!FieldType).length != 0;
+
+    mixin("@property void " ~ name ~ "(" ~ FieldType.stringof ~ " newValue)
+        {
+            static if (hasMin)
+            {
+                newValue = newValue < getUDAs!(Field, cereslib.properties.min!(typeof(Field)))[0].value
+                    ? getUDAs!(Field, cereslib.properties.min)[0].value
+                    : newValue;
+            }
+
+            static if (hasMax)
+            {
+                newValue = newValue > getUDAs!(Field, cereslib.properties.max!(typeof(Field)))[0].value
+                    ? getUDAs!(Field, cereslib.properties.max)[0].value
+                    : newValue;
+            }
+
+            " ~ fieldName ~ " = newValue;
+        }"
+    );
+}
+
+unittest
+{
+    struct Test
+    {
+        private @min!float(5.5f) float value_;
+        private @max!uint(4u) uint value2_;
+
+        mixin MakeGetter!value_;
+        mixin MakeSetter!value_;
+
+        mixin MakeGetter!value2_;
+        mixin MakeSetter!value2_;
+    }
+
+    Test test;
+
+    test.value = 2.0f;
+    assert(test.value == 5.5f);
+
+    test.value = 10.0f;
+    assert(test.value == 10.0f);
+
+    test.value2 = 100;
+    assert(test.value2 == 4);
+}
